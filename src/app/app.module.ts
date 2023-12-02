a72fc885a2937d7e90933c954bc2cf1015ed4e1e
Felipe Mendes <felipemadruga@gmail.com>
2023-12-02 19:37:32 -0300

    implementa shipping component e funcionalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
@@ -9,6 +10,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
@@ -16,17 +18,20 @@ import { CartComponent } from './cart/cart.component';
     ReactiveFormsModule,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent,
+    ShippingComponent,
+    HttpClientModule,
     RouterModule.forRoot([
+      { path: 'cart', component: CartComponent},
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent},
-      { path: 'cart', component: CartComponent}
+      { path: 'shipping', component: ShippingComponent},
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
+    CartComponent
   ],
   bootstrap: [
     AppComponent
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { CartService } from '../cart.service';
-import { Product } from '../products';
 
 @Component({
   selector: 'app-cart',
-  standalone: true,
-  imports: [CommonModule],
+  // standalone: true,
+  // imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
@@ -15,7 +14,6 @@ export class CartComponent {
   items = this.cartService.getItems();
 
   constructor(
-    private route: ActivatedRoute,
     private cartService: CartService) {
 
   }
