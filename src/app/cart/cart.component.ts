e9240c2d49236cb666b99d482b479c2ab8713ef2
Felipe Mendes <felipemadruga@gmail.com>
2023-12-05 19:18:20 -0300

    Inclui forms
    Passo de forms do tutorial get started angular

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
-  // standalone: true,
-  // imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
   constructor(
-    private cartService: CartService) {
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+    ) { }
 
+
+
+  onSubmit(): void {
+    this.items = this.cartService.clearItems();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
-
 }
